c3e127e8115ab542d28dc21a3026c699c106ce77
AleshaD <shadrov.al@gmail.com>
2021-11-18 09:35:17 +0300

    change sightType to String

--- lib/domain/sight.dart
+++ lib/domain/sight.dart
@@ -1,20 +1,10 @@
-
-enum SightType {
-  military,
-  culture,
-  entertainment,
-  religion,
-  natural,
-  hystoric
-}
-
 class Sight {
   final String name;
   final double lat;
   final double lon;
   final String url;
   final String details;
-  final SightType type;
+  final String type;
   const Sight(
       {required this.name,
       required this.lat,
