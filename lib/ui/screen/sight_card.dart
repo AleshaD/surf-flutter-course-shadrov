5d7ccd70cc07a826f300c76a889cf8a73c9269f6
AleshaD <shadrov.al@gmail.com>
2021-11-23 16:25:26 +0300

    use aspectratio for sightcard

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -8,67 +8,75 @@ class SightCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Stack(
-        children: [
-          Container(
-            // Виджет под фотографию
-            height: 96,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                borderRadius:
-                    BorderRadius.only(topLeft: Radius.circular(cornerRadius), topRight: Radius.circular(cornerRadius)),
-                color: Colors.amber),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                sight.type,
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700, color: Colors.white),
+    return AspectRatio(
+      aspectRatio: 3/2,
+      child: Column(children: [
+        Expanded(
+          flex: 3,
+          child: Stack(
+            children: [
+              Container(
+                // Виджет под фотографию
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(cornerRadius), topRight: Radius.circular(cornerRadius)),
+                    color: Colors.amber),
               ),
-            ),
-          ),
-          Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: EdgeInsets.only(right: 18, top: 12),
-                child: Icon(
-                  Icons.favorite_border_outlined,
-                  color: Colors.white,
-                  size: 26,
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    sight.type,
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700, color: Colors.white),
+                  ),
                 ),
-              )),
-        ],
-      ),
-      Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            color: Color(0xffF5F5F5),
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(cornerRadius), bottomRight: Radius.circular(cornerRadius))),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 200),
-              child: Text(
-                sight.name,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 2),
-              child: Text(
-                'краткое описание',
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xff7C7E92)),
-              ),
-            )
-          ],
+              Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 18, top: 12),
+                    child: Icon(
+                      Icons.favorite_border_outlined,
+                      color: Colors.white,
+                      size: 26,
+                    ),
+                  )),
+            ],
+          ),
         ),
-      )
-    ]);
+        Expanded(
+          flex: 2,
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: Color(0xffF5F5F5),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(cornerRadius), bottomRight: Radius.circular(cornerRadius))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 200),
+                  child: Text(
+                    sight.name,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 2),
+                  child: Text(
+                    'краткое описание',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xff7C7E92)),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
   }
 }
