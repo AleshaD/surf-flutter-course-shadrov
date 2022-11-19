566f06e4a1cee3032862c663e990e95d4600428a
Alexey Shadrov <Shadrov.al@gmail.com>
2022-11-19 19:27:24 +0300

    fix unload image exeptions from flutter

--- lib/ui/widgets/network_image_with_progress.dart
+++ lib/ui/widgets/network_image_with_progress.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class NetworkImageWithProgress extends StatelessWidget {
+import '../../data/repository/sight_images_repository.dart';
+
+class NetworkImageWithProgress extends StatefulWidget {
   ///Create Image.network and show CircularProgressIndicator while loading
   const NetworkImageWithProgress(this.url, {this.fit = BoxFit.cover});
 
@@ -8,23 +12,39 @@ class NetworkImageWithProgress extends StatelessWidget {
   final String url;
 
   @override
-  Widget build(BuildContext context) {
-    return Image.network(
-      url,
-      fit: fit,
-      loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-        if (loadingProgress == null) {
-          return child;
-        }
+  State<NetworkImageWithProgress> createState() => _NetworkImageWithProgressState();
+}
 
-        return Center(
-          child: CircularProgressIndicator(
-            value: loadingProgress.expectedTotalBytes != null
-                ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                : null,
-          ),
+class _NetworkImageWithProgressState extends State<NetworkImageWithProgress> {
+  final String noImageAsset = 'assets/images/image_not_found.jpeg';
+  Widget? imageWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return imageWidget ??
+        FutureBuilder(
+          future: SightImagesRepository.instance.getImage(widget.url),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                final File? file = snapshot.data as File?;
+                if (file != null) {
+                  imageWidget = Image.file(file, fit: widget.fit);
+                  return imageWidget!;
+                } else {
+                  imageWidget = Image.asset(noImageAsset, fit: widget.fit);
+                  return imageWidget!;
+                }
+              } else {
+                imageWidget = Image.asset(noImageAsset, fit: widget.fit);
+                return imageWidget!;
+              }
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         );
-      },
-    );
   }
 }
