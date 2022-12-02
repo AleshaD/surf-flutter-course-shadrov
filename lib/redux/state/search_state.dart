74a318ae1596351989a114f10b93190f45d905a0
Alexey Shadrov <Shadrov.al@gmail.com>
2022-12-02 19:57:27 +0300

    add redux search state

---
+++ lib/redux/state/search_state.dart
@@ -0,0 +1,46 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:places/data/model/sights/searched_sight.dart';
+
+part 'search_state.freezed.dart';
+
+@freezed
+class SearchState with _$SearchState {
+  const SearchState._();
+
+  String get errorMsg => maybeWhen<String>(
+        erorr: (_, __, errorMsg) => errorMsg,
+        orElse: () => '',
+      );
+
+  bool get hasError => errorMsg.isNotEmpty;
+
+  bool get inProgress => maybeWhen<bool>(
+        inProgress: (_, __) => true,
+        orElse: () => false,
+      );
+
+  /// Начальное состояние
+  const factory SearchState.init({
+    @Default([]) List<SearchedSight> sigths,
+    @Default({}) Set<String> history,
+  }) = _Init;
+
+  /// В процессе загрузки
+  const factory SearchState.inProgress({
+    @Default([]) List<SearchedSight> sigths,
+    @Default({}) Set<String> history,
+  }) = _InProgress;
+
+  /// Ошибка при поиске
+  const factory SearchState.erorr({
+    @Default([]) List<SearchedSight> sigths,
+    @Default({}) Set<String> history,
+    @Default('Ошибка при поиске') String errorMessage,
+  }) = _Erorr;
+
+  /// Поиск выполнен
+  const factory SearchState.result({
+    @Default([]) List<SearchedSight> sigths,
+    @Default({}) Set<String> history,
+  }) = _Result;
+}
