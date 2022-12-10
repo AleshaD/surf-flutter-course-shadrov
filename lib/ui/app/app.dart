2721638cbb4bc04cec2479a9497a091aae5d2f43
Alexey Shadrov <Shadrov.al@gmail.com>
2022-12-10 23:09:27 +0300

    start app from home

--- lib/ui/app/app.dart
+++ lib/ui/app/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:places/constants/app_strings.dart';
 import 'package:places/res/app_theme_config.dart';
-import 'package:places/ui/screen/onboarding_screen/onboarding_screen_widget.dart';
+import 'package:places/ui/screen/home_screen.dart/home_screen.dart';
 
 
 class App extends StatefulWidget {
@@ -34,7 +34,7 @@ class AppState extends State<App> {
       supportedLocales: [const Locale('en'), const Locale('ru')],
       title: AppStrings.appTitle,
       theme: _appThemeConfig.data,
-      home: OnboardingScreenWidget(),
+      home: HomeScreen(),
     );
   }
 }
