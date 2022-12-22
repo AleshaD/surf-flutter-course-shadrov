5344d5d60e1d8537d2a23a8277694dc92ec101a1
Alexey Shadrov <Shadrov.al@gmail.com>
2022-12-22 13:23:46 +0300

    rotate logo in splash screen

--- lib/ui/screen/splash_screen/splash_screen.dart
+++ lib/ui/screen/splash_screen/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as Math;
+
 import 'package:flutter/material.dart';
 import 'package:places/styles/custom_icons.dart';
 import 'package:places/ui/screen/onboarding_screen/onboarding_screen_widget.dart';
@@ -9,20 +11,47 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
   final double centerCircleSize = 160;
+  final int rotateInMs = 1500;
+  late final AnimationController _logoAnimationCtrl;
+  late final Animation<double> _rotateAnimation;
 
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 2), () {
+    Future.delayed(Duration(milliseconds: rotateInMs * 3 + 500), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (context) => OnboardingScreenWidget(),
         ),
       );
     });
+
+    _logoAnimationCtrl = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: rotateInMs),
+    );
+    _logoAnimationCtrl.repeat();
+
+    _rotateAnimation = Tween<double>(
+      begin: 0,
+      end: 360,
+    ).animate(
+      CurvedAnimation(
+        parent: _logoAnimationCtrl,
+        curve: Curves.easeInOut,
+      ),
+    );
   }
+
+  @override
+  void dispose() {
+    _logoAnimationCtrl.dispose();
+    super.dispose();
+  }
+
+  double _degreeToRadian(double degree) => degree * Math.pi / 180;
 
   @override
   Widget build(BuildContext context) {
@@ -38,10 +67,19 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
       ),
       child: Center(
-        child: Icon(
-          CustomIcons.spash_screen_logo,
-          size: 160,
-          color: Colors.white, // Theme.of(context).colorScheme.secondary,
+        child: AnimatedBuilder(
+          animation: _logoAnimationCtrl,
+          builder: (context, child) {
+            final degree = _rotateAnimation.value;
+            return Transform.rotate(
+              angle: _degreeToRadian(degree) * -1,
+              child: Icon(
+                CustomIcons.spash_screen_logo,
+                size: 160,
+                color: Colors.white,
+              ),
+            );
+          },
         ),
       ),
     );
