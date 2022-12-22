644f9dc43db8be4d5abb4b99e775889529fec472
Alexey Shadrov <Shadrov.al@gmail.com>
2022-12-22 13:50:03 +0300

    animate central icons in onboarding pages

--- lib/ui/screen/onboarding_screen/widgets/onboarding_page.dart
+++ lib/ui/screen/onboarding_screen/widgets/onboarding_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({
     required this.iconData,
     required this.title,
@@ -12,6 +12,36 @@ class OnboardingPage extends StatelessWidget {
   final String subtitle;
 
   @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> with SingleTickerProviderStateMixin {
+  late final AnimationController _iconSizeAnimationCtrl;
+  final double maxIconSize = 100;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconSizeAnimationCtrl = AnimationController(
+      vsync: this,
+      lowerBound: 0,
+      upperBound: maxIconSize,
+      duration: Duration(milliseconds: 400),
+    );
+
+    Future.delayed(
+      Duration.zero,
+      () => _iconSizeAnimationCtrl.forward(from: 0),
+    );
+  }
+
+  @override
+  void dispose() {
+    _iconSizeAnimationCtrl.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 58),
@@ -19,16 +49,22 @@ class OnboardingPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Icon(
-            iconData,
-            size: 100,
-            color: Theme.of(context).primaryColor,
+          SizedBox(
+            height: maxIconSize,
+            child: AnimatedBuilder(
+              animation: _iconSizeAnimationCtrl,
+              builder: (_, __) => Icon(
+                widget.iconData,
+                size: _iconSizeAnimationCtrl.value,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
           ),
           SizedBox(
             height: 48,
           ),
           Text(
-            title,
+            widget.title,
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.headline5,
           ),
@@ -36,12 +72,11 @@ class OnboardingPage extends StatelessWidget {
             height: 8,
           ),
           Text(
-            subtitle,
+            widget.subtitle,
             textAlign: TextAlign.center,
-            style: Theme.of(context)
-                .textTheme
-                .subtitle2!
-                .copyWith(color: Theme.of(context).primaryColor),
+            style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                  color: Theme.of(context).primaryColor,
+                ),
           ),
         ],
       ),
