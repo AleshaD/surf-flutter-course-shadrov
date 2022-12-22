0cee863241329b19a74745440952ae1f973882f6
Alexey Shadrov <Shadrov.al@gmail.com>
2022-12-22 18:04:22 +0300

    fix blinking images in pageview in details screen

--- lib/ui/widgets/photo_page_view/photo_page_view.dart
+++ lib/ui/widgets/photo_page_view/photo_page_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:places/ui/widgets/network_image_with_progress.dart';
 import 'package:places/ui/widgets/photo_page_view/photo_page_pagination.dart';
@@ -20,35 +22,20 @@ class PhotoPageView extends StatefulWidget {
 class _PhotoPageViewState extends State<PhotoPageView> {
   double clipCornerRadius = 0;
   int currentPage = 0;
+  final _pageChangeStreamCtrl = StreamController<int>();
 
   final _pageController = PageController();
-
-  @override
-  void dispose() {
-    super.dispose();
-    _pageController.dispose();
-  }
 
   @override
   void initState() {
     super.initState();
     clipCornerRadius = widget.topCornerRadius;
-    _pageController.addListener(_straightenTopCornersWhenMoving);
   }
 
-  void _straightenTopCornersWhenMoving() {
-    bool pageOnMoving = _pageController.page! % 1 != 0;
-    if (pageOnMoving && clipCornerRadius != 0) {
-      setState(() {
-        clipCornerRadius = 0;
-      });
-    }
-
-    if (!pageOnMoving && clipCornerRadius != widget.topCornerRadius) {
-      setState(() {
-        clipCornerRadius = widget.topCornerRadius;
-      });
-    }
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -61,29 +48,27 @@ class _PhotoPageViewState extends State<PhotoPageView> {
             controller: _pageController,
             allowImplicitScrolling: true,
             itemCount: widget.photoUrls.length,
-            onPageChanged: (value) => setState(() {
-              currentPage = value;
-            }),
+            onPageChanged: (value) => _pageChangeStreamCtrl.add(value),
             itemBuilder: (context, index) {
-              return ClipRRect(
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(
-                    clipCornerRadius,
-                  ),
-                ),
-                child: NetworkImageWithProgress(
-                  widget.photoUrls[index],
-                ),
+              return NetworkImageWithProgress(
+                widget.photoUrls[index],
               );
             },
           ),
           widget.photoUrls.length > 1
-              ? Align(
-                  alignment: Alignment.bottomCenter,
-                  child: PhotoPagePagination(
-                    pagesCount: widget.photoUrls.length,
-                    currentPage: currentPage,
-                  ),
+              ? StreamBuilder<int>(
+                  stream: _pageChangeStreamCtrl.stream,
+                  initialData: 0,
+                  builder: (context, snapshot) {
+                    final currentPage = snapshot.data!;
+                    return Align(
+                      alignment: Alignment.bottomCenter,
+                      child: PhotoPagePagination(
+                        pagesCount: widget.photoUrls.length,
+                        currentPage: currentPage,
+                      ),
+                    );
+                  },
                 )
               : Container()
         ],
