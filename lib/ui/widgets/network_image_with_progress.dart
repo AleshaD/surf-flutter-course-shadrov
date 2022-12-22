c30c7423b03edda23a285d8491ef78120c7d2de6
Alexey Shadrov <Shadrov.al@gmail.com>
2022-12-22 20:42:31 +0300

    fade in image after circular loading

--- lib/ui/widgets/network_image_with_progress.dart
+++ lib/ui/widgets/network_image_with_progress.dart
@@ -14,25 +14,57 @@ class NetworkImageWithProgress extends StatefulWidget {
 }
 
 class _NetworkImageWithProgressState extends State<NetworkImageWithProgress> {
+  Image? imageWidget;
+  CrossFadeState animatedState = CrossFadeState.showFirst;
+  final loadingState = CrossFadeState.showFirst;
+  final imageState = CrossFadeState.showSecond;
+
+  @override
+  void initState() {
+    super.initState();
+    imageWidget = context.read<SightImagesInteractor>().getImageSync(
+          url: widget.url,
+        );
+    if (imageWidget == null) {
+      animatedState = loadingState;
+      Future.delayed(Duration.zero, _loadImage);
+    } else {
+      animatedState = imageState;
+    }
+  }
+
+  void _loadImage() async {
+    imageWidget = await context.read<SightImagesInteractor>().getImageFrom(
+          url: widget.url,
+        );
+    setState(() {
+      animatedState = imageState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final Image? imageWidget =
-        context.read<SightImagesInteractor>().getImageSync(url: widget.url);
-
-    return imageWidget ??
-        FutureBuilder(
-          future: context.read<SightImagesInteractor>().getImageFrom(url: widget.url),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.connectionState == ConnectionState.done) {
-              return snapshot.hasData ? snapshot.data as Image : SizedBox.shrink();
-            } else {
-              return SizedBox.shrink();
-            }
-          },
+    return AnimatedCrossFade(
+      duration: Duration(milliseconds: 500),
+      crossFadeState: animatedState,
+      layoutBuilder: (topChild, topChildKey, bottomChild, bottomChildKey) {
+        return Stack(
+          children: <Widget>[
+            Positioned.fill(
+              key: bottomChildKey,
+              child: bottomChild,
+            ),
+            Positioned.fill(
+              key: topChildKey,
+              child: topChild,
+            ),
+          ],
         );
+      },
+      firstChild: Center(
+        child: CircularProgressIndicator(),
+      ),
+      secondChild: imageWidget ?? SizedBox.shrink(),
+    );
   }
 }
