6889ef081bdaf09ca6abd5b10e909c1f4a1324ee
Alexey Shadrov <Shadrov.al@gmail.com>
2023-01-04 13:58:54 +0300

    set and get sight filter from settings repo

--- lib/ui/screen/filters_screen/filter_screen_model.dart
+++ lib/ui/screen/filters_screen/filter_screen_model.dart
@@ -4,23 +4,29 @@ import 'package:elementary/elementary.dart';
 import 'package:places/data/model/exceptions/network_exceptions.dart';
 import 'package:places/data/model/sights/sight.dart';
 import 'package:places/data/model/sights/sight_filter.dart';
+import 'package:places/data/repository/settings_repository.dart';
 import 'package:places/data/repository/sight_repository.dart';
 
 class FilterScreenModel extends ElementaryModel {
   FilterScreenModel(
     ErrorHandler errorHandler, {
     required SightRepository sightRepository,
+    required SettingsRepository settingsRepository,
   })  : this._sightRepository = sightRepository,
+        this._settingsRepository = settingsRepository,
         super(errorHandler: errorHandler);
 
   final SightRepository _sightRepository;
+  final SettingsRepository _settingsRepository;
   late final StreamSubscription<NetworkExceptions> _networkRepoErrorSubscription;
 
   void updateUserSightFilter(SightFilter filter) {
-    _sightRepository.userFilter = filter;
+    _settingsRepository.setSightFilter(filter);
   }
 
-  SightFilter getUserSightFilter() => _sightRepository.userFilter;
+  SightFilter getUserSightFilter() {
+    return _settingsRepository.getSightFilter();
+  }
 
   Future<List<Sight>> getSightFromFilter(SightFilter filter) async {
     final sights = await _sightRepository.getSightsFromFilter(filter);
