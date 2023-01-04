4896f81b4be562022a0ae19ec41bbe8ffd375a8d
Alexey Shadrov <Shadrov.al@gmail.com>
2023-01-04 11:35:20 +0300

    set watch onboarding pages

--- lib/ui/screen/onboarding_screen/onboarding_screen_model.dart
+++ lib/ui/screen/onboarding_screen/onboarding_screen_model.dart
@@ -1,5 +1,12 @@
 import 'package:elementary/elementary.dart';
+import 'package:places/data/repository/settings_repository.dart';
 
 class OnboardingScreenModel extends ElementaryModel {
-  OnboardingScreenModel() : super();
+  OnboardingScreenModel(this._settingsRepository) : super();
+
+  final SettingsRepository _settingsRepository;
+
+  void setWatchedOnboarding() {
+    _settingsRepository.setWatchedOnboarding();
+  }
 }
