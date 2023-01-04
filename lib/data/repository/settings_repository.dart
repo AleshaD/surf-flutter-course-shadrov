db663afdf32c4d9ad62a4a06c774518682c8cc66
Alexey Shadrov <Shadrov.al@gmail.com>
2023-01-04 11:24:06 +0300

    add settings repository

---
+++ lib/data/repository/settings_repository.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:places/data/model/sights/sight_filter.dart';
+import 'package:places/data/model/enums/theme_type.dart';
+import 'package:places/data/providers/local_storage.dart';
+import 'package:places/data/repository/i_settings_repository.dart';
+
+class SettingsRepository implements ISettingsRepository {
+  SettingsRepository(this._storage);
+
+  final LocalStorage _storage;
+
+  @override
+  bool getIsWatchedOnboarding() {
+    return _storage.getIsWatchedOnboarding();
+  }
+
+  @override
+  void setWatchedOnboarding() {
+    _storage.setIsWatchedOnboarding();
+  }
+
+  @override
+  SightFilter getSightFilter() {
+    final filterJson = _storage.getSightFilterJson();
+    if (filterJson != null)
+      return SightFilter.fromJson(json.decode(filterJson));
+    else
+      return SightFilter.dafult();
+  }
+
+  @override
+  void setSightFilter(SightFilter filter) {
+    _storage.setSightFilterJson(
+      json.encode(
+        filter.toJson(),
+      ),
+    );
+  }
+
+  @override
+  ThemeType getThemeType() {
+    final themeIndex = _storage.getThemeIndex();
+    return ThemeType.values[themeIndex];
+  }
+
+  @override
+  void setThemeType(ThemeType type) {
+    _storage.setThemeIndex(type.index);
+  }
+}
