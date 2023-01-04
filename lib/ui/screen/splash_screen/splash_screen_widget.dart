85decb2956e44c7d25f535286768958452d569af
Alexey Shadrov <Shadrov.al@gmail.com>
2023-01-04 11:26:23 +0300

    add splash screen widget

---
+++ lib/ui/screen/splash_screen/splash_screen_widget.dart
@@ -0,0 +1,43 @@
+import 'package:elementary/elementary.dart';
+import 'package:flutter/material.dart';
+import 'package:places/styles/custom_icons.dart';
+import 'splash_screen_wm.dart';
+
+class SplashScreenWidget extends ElementaryWidget<ISplashScreenWidgetModel> {
+  const SplashScreenWidget({
+    Key? key,
+    WidgetModelFactory wmFactory = defaultSplashScreenWidgetModelFactory,
+  }) : super(wmFactory, key: key);
+
+  @override
+  Widget build(ISplashScreenWidgetModel wm) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment(-2.28, -1),
+          end: Alignment.topRight,
+          colors: [
+            wm.theme.colorScheme.secondaryContainer,
+            wm.theme.colorScheme.secondary
+          ],
+        ),
+      ),
+      child: Center(
+        child: AnimatedBuilder(
+          animation: wm.logoAnimationCtrl,
+          builder: (context, child) {
+            final degree = wm.rotateAnimation.value;
+            return Transform.rotate(
+              angle: wm.degreeToRadian(degree) * -1,
+              child: Icon(
+                CustomIcons.spash_screen_logo,
+                size: 160,
+                color: Colors.white,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
