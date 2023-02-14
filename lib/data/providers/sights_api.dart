8862ce4d921f13f7427870ec060573d480b1e20e
Alexey Shadrov <Shadrov.al@gmail.com>
2023-02-14 20:43:25 +0300

    change sight api for upload media method

--- lib/data/providers/sights_api.dart
+++ lib/data/providers/sights_api.dart
@@ -4,14 +4,19 @@ import 'package:dio/dio.dart';
 import 'package:places/data/model/sights/sight_dto.dart';
 import 'package:places/data/model/sights/sights_filter_request_dto.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:http_parser/http_parser.dart';
 
 import '../model/sights/sight.dart';
 
 part 'sights_api.g.dart';
 
-@RestApi(baseUrl: 'https://test-backend-flutter.surfstudio.ru')
+const String surfBaseUrl = 'https://test-backend-flutter.surfstudio.ru';
+
+@RestApi(baseUrl: surfBaseUrl)
 abstract class SightsApi {
   factory SightsApi(Dio dio, {String baseUrl}) = _SightsApi;
+
+  static const String baseUrl = surfBaseUrl;
 
   @POST('/filtered_places')
   Future<List<SightDto>> getFilteredSights(@Body() SightsFilterRequestDto filter);
@@ -31,8 +36,15 @@ abstract class SightsApi {
   @PUT('/place/{id}')
   Future<Sight> updateSight(@Path() int id, @Body() Map<String, dynamic> updateData);
 
+  /// Сохранение файлов в хранилище.
+  /// В ответ приходит путь к загруженному файлу, относительно текущего домена.
+  /// Данные передаются в multipart/form-data
+  /// Допустимые типы файлов: jpg, png, gif, svg.
+  /// Если загружен один файл, то путь к нему содержится в заголовке "location".
+  /// Если в запросе было больше одного файла, то ответ в виде списка путей к загруженным файлам в теле ответа.
+  /// Загружаем только один файл
   @POST('/upload_file')
-  Future<String> mediaUpload(@Part() File file);
+  Future<HttpResponse<String>> mediaUpload(@Part(contentType: 'image/jpeg') File file);
 
   @GET('/files/{path}')
   Future<String> fetchFile(@Path() String path);
