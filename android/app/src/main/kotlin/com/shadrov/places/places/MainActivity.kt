d34fd441864c076cac9dc751f487b3aee596c930
Alexey Shadrov <Shadrov.al@gmail.com>
2023-02-16 11:50:46 +0300

    setup android map and location

--- android/app/src/main/kotlin/com/shadrov/places/places/MainActivity.kt
+++ android/app/src/main/kotlin/com/shadrov/places/places/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.shadrov.places.places
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    MapKitFactory.setApiKey("7ae9989f-69fd-4b06-b45a-13232c0a0e85")
+    super.configureFlutterEngine(flutterEngine)
+  }
 }
