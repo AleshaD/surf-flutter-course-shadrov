612457d8412a44ba9fe8dabbfb18228e286cbf28
Alexey Shadrov <Shadrov.al@gmail.com>
2023-02-18 14:26:16 +0300

    add sights map model

---
+++ lib/ui/screen/sights_map_screen/sights_map_model.dart
@@ -0,0 +1,33 @@
+import 'package:elementary/elementary.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:places/data/model/coordinate.dart';
+import 'package:places/data/model/sights/sight.dart';
+import 'package:places/data/repository/location_repository.dart';
+import 'package:places/data/repository/sight_repository.dart';
+
+class SightsMapModel extends ElementaryModel {
+  SightsMapModel(
+    ErrorHandler errorHandler,
+    LocationRepository locationRepository,
+    SightRepository sightRepository,
+  )   : this._locationRepository = locationRepository,
+        this._sightRepository = sightRepository,
+        super(errorHandler: errorHandler);
+
+  final LocationRepository _locationRepository;
+  final SightRepository _sightRepository;
+
+  Future<Coordinate?> getUserCoordinate() async {
+    return await _locationRepository.getCurrentOrPreviousCoordinate();
+  }
+
+  Future<Position?> getUserPosition() async {
+    return await _locationRepository.getUserPosition();
+  }
+
+  Future<List<Sight>> getSights(
+    Coordinate? coordinate,
+  ) async {
+    return await _sightRepository.getSightsWithUserChosenFilter(coordinate);
+  }
+}
